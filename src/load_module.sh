99ac05fc3906456dd7e6424272b69249dfe08b3c
Bruce Hill <bhill@slac.stanford.edu>
2022-11-17 19:32:35 -0800

    Rework src/load_module.sh to work in different shells.
    Also made easier to read and added more diag msgs.

--- src/load_module.sh
+++ src/load_module.sh
@@ -1,22 +1,42 @@
 #!/bin/sh
 
+rmmod -s evr_device.ko
+rmmod -s tpr.ko
 insmod tpr.ko
 
-/bin/rm -f /dev/tpr[a-z]*
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%cBSA c %d 15\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c    c %d 14\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c0   c %d  0\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c1   c %d  1\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c2   c %d  2\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c3   c %d  3\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c4   c %d  4\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c5   c %d  5\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c6   c %d  6\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c7   c %d  7\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c8   c %d  8\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%c9   c %d  9\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%ca   c %d 10\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%cb   c %d 11\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%cc   c %d 12\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
-/usr/bin/awk 'BEGIN{n=97;}/tpr/{printf "/bin/mknod -m 666 /dev/tpr%cd   c %d 13\n", n++, $1}' /proc/devices | xargs -I % /bin/sh -c '%'
+# Search for TPR devices
+majors=$(awk "\$2==\"tpr\" {print \$1}" /proc/devices)
+echo "Found" $(echo $majors | wc -w) "TPR Event Receivers."
+
+echo "Creating device nodes..."
+umask 022
+
+if [ -e /dev/tpr ]; then
+	rm -f /dev/tpr
+fi
+device=1
+for major in $majors; do
+    dev=$(echo $device | awk '{ printf "%c", 96+ $1}')
+    device=$((++device))
+    echo "Creating nodes /dev/tpr"$dev"* for major" $major
+    if [ -e /dev/tpr$dev ]; then
+		rm -f /dev/tpr$dev*
+	fi
+    mknod -m 666 '/dev/tpr'$devBSA c $major 15
+    mknod -m 666 '/dev/tpr'$dev    c $major 14
+    mknod -m 666 '/dev/tpr'$dev'0' c $major 0
+    mknod -m 666 '/dev/tpr'$dev'1' c $major 1
+    mknod -m 666 '/dev/tpr'$dev'2' c $major 2
+    mknod -m 666 '/dev/tpr'$dev'3' c $major 3
+    mknod -m 666 '/dev/tpr'$dev'4' c $major 4
+    mknod -m 666 '/dev/tpr'$dev'5' c $major 5
+    mknod -m 666 '/dev/tpr'$dev'6' c $major 6
+    mknod -m 666 '/dev/tpr'$dev'7' c $major 7
+    mknod -m 666 '/dev/tpr'$dev'8' c $major 8
+    mknod -m 666 '/dev/tpr'$dev'9' c $major 9
+    mknod -m 666 '/dev/tpr'$dev'a' c $major 10
+    mknod -m 666 '/dev/tpr'$dev'b' c $major 11
+    mknod -m 666 '/dev/tpr'$dev'c' c $major 12
+    mknod -m 666 '/dev/tpr'$dev'd' c $major 13
+done
 
